d4fb7504bccdb5d5f5c03394abf10770753a0089
Sebastian <ascacere92@gmail.com>
2020-07-13 17:07:46 -0500

    Added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Running the project
+ - Download `pyenv`
+ - Install the dependencies using it.
+ - Activate the environment using `pyenv shell`
+ - cp `.env.example` to `.env`
+ - Change the variables
+ - Launch using python primadonna.py
--- primadonna.py
+++ primadonna.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -11,6 +12,7 @@ load_dotenv(find_dotenv())
 USERNAME = os.getenv("SIGAT_USER")
 PASSWORD = os.getenv("PASSWORD")
 SEMANA = os.getenv("SEMANA")
+FINCA = os.getenv("FINCA")
 
 
 WAIT_TIME = 20
@@ -35,7 +37,12 @@ browser.title
 finca = browser.find_element_by_id('client')
 finca.click()
 
-finca.find_element_by_xpath("//option[@value='{}']".format(SEMANA)).click()
+try:
+    WebDriverWait(browser, WAIT_TIME).until(EC.element_to_be_clickable((By.XPATH, "//option[@value='{}']".format(FINCA))))
+except (TimeoutException):
+    print('skip this')
+
+finca.find_element_by_xpath("//option[@value='{}']".format(FINCA)).click()
 
 browser.implicitly_wait(WAIT_TIME)  # seconds
 
@@ -50,7 +57,7 @@ semana = browser.find_element_by_id('semana')
 """
 Here we change the week
 """
-semana.find_element_by_xpath("//option[@value='26']").click()
+semana.find_element_by_xpath("//option[@value='{}']".format(SEMANA)).click()
 browser.implicitly_wait(WAIT_TIME) # seconds
 
 send_btn = browser.find_element_by_xpath("//button[@class='btn green']")
